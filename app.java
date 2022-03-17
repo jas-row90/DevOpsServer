d6dd7cb2e636434e1d07104a99aecddcb1e25a6b
Jasmine Rowlett <jazyorow@gmail.com>
2022-03-17 23:03:01 +0000

    my first commit

---
+++ app.java
@@ -0,0 +1,6 @@
+#This is a test file created in a git repository under the UbuntuServer
+echo "Please enter your name"
+read name
+echo "Please enter your password"
+read -s password  
+echo "Welcome to the zelle application"
